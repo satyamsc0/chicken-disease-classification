02ee6a76906899beeaf16b1d6fe60254ec74f2a2
satyamsc0 <satyamsc0@gmail.com>
2024-05-21 01:48:07 +0530

    added dockerfile

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def home():
 @app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
-    os.system("dvc repro")     # ("python main.py")
+    os.system("python main.py")     # ("dvc repro")
     return "Training done successfully!"
 
 
@@ -43,6 +43,6 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080) #local host
+    # app.run(host='0.0.0.0', port=8080) #local host
     # app.run(host='0.0.0.0', port=8080) #for AWS
-    # app.run(host='0.0.0.0', port=80) #for AZURE
+    app.run(host='0.0.0.0', port=80) #for AZURE
