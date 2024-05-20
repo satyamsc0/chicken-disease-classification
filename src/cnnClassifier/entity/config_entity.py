834536fd29d2c649019d874d8b201dcf75f000a6
satyamsc0 <satyamsc0@gmail.com>
2024-05-20 11:51:04 +0530

    prepare base model completed

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -9,3 +9,15 @@ class DataIngestionConfig:
     unzip_dir: Path
     # all the above params are same as in config.yaml
     
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
